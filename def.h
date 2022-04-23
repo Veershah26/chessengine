6c73cc0bc864c696049c873c3104cdb3db68db49
VeerShah26 <shah.veer18@gmail.com>
2022-04-23 16:56:03 +0530

    defined pieces, rank and file through enum ✅

--- def.h
+++ def.h
@@ -6,4 +6,9 @@ typedef unsigned long long U64;
 #define NAME "Engine 1.0"
 #define BOARD_SQUARE_NUMBER 120
 
+enum { EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK };
+enum { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE };
+enum { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE };
+
+
 #endif
